76083606a88b7422c099d5a5af3561cba0e03ed5
Ruyi Lan <rliu2400@gmail.com>
2025-01-11 20:11:11 -0600

    updated

--- app/page.js
+++ app/page.js
@@ -1,100 +1,103 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import getStripe from '@/utils/get-stripe'
-import { SignedIn,SignedOut, UserButton, UserProfile } from "@clerk/nextjs"
-import { Link, AppBar, Toolbar, Typography, Container, Button, Box, Grid, CardContent, Card, CardActionArea } from "@mui/material";
+import getStripe from "@/utils/get-stripe";
+import {
+  Typography,
+  Container,
+  Button,
+  Box,
+  Grid,
+  CardContent,
+  Card,
+  CardActionArea,
+} from "@mui/material";
 
-import { useState, useRef } from "react"
-import { useUser } from "@clerk/nextjs"
+import { useState, useRef } from "react";
+import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import Header from "./header"
+import Header from "./header";
 
 export default function Home() {
-    const {isLoaded,isSignedIn,user} = useUser()
-    const router = useRouter()
-    const [subscribed,setSubscribed] = useState(false)
+  const { isLoaded, isSignedIn, user } = useUser();
+  const router = useRouter();
+  const [subscribed, setSubscribed] = useState(false);
 
-    const str=`
-      [
-        {
-          "id": 1,
-          "color": "pink",
-          "front": "Easy text input",
-          "back": "Simply input your text and let our software do the rest. Creating flashcards has never been easier."
-        },
-        {
-          "id": 2,
-          "color": "lightgrey",          
-          "front": "Smart flashcards",
-          "back": "Our AI intelligently breaks down your text into concise flashcards, perfect for studying."
-        },
-        {
-          "id": 3,
-          "color": "lightgreen",          
-          "front": "Accessible anywhere",
-          "back": "Access your flashcards from any device, at any time. Study on the go with ease."
-        }              
-      ]
-      `
+  const str = `
+  [
+    {
+      "id": 1,
+      "color": "pink",
+      "front": "Effortless Flashcard Creation",
+      "back": "Type in your content, and our smart system instantly transforms it into structured flashcards. Studying has never been this simple."
+    },
+    {
+      "id": 2,
+      "color": "lightgrey",          
+      "front": "AI-Powered Learning",
+      "back": "Our AI analyzes your text and generates concise, easy-to-review flashcards, helping you retain information faster."
+    },
+    {
+      "id": 3,
+      "color": "lightgreen",          
+      "front": "Study Anytime, Anywhere",
+      "back": "Sync your flashcards across all your devices and study on the go, whether you're at home, in class, or commuting."
+    }              
+  ]
+`;
 
-  const [flashcards,setFlashcards] = useState(JSON.parse(str))
-  const [flipped,setFlipped] = useState([])
+  const [flashcards, setFlashcards] = useState(JSON.parse(str));
+  const [flipped, setFlipped] = useState([]);
 
   const handleCardClick = (id) => {
     setFlipped((prev) => ({
-        ...prev,
-        [id]: !prev[id]
-    }))
-}
-
-
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
 
   const handleSubmit = async (amount) => {
     if (!user) {
-      router.push(`/sign-in`)  
+      router.push(`/sign-in`);
     } else {
-      const checkoutSession = await fetch('/api/checkout_session',{
+      const checkoutSession = await fetch("/api/checkout_session", {
         method: "POST",
         headers: {
-          origin: 'http://localhost:3000',
+          origin: "http://localhost:3000",
           amount: amount,
         },
-      })
+      });
 
-      const checkoutSessionJson = await checkoutSession.json()
+      const checkoutSessionJson = await checkoutSession.json();
 
       if (checkoutSession.statusCode === 500) {
-        console.error(checkoutSession.message)
-        return
+        console.error(checkoutSession.message);
+        return;
       }
 
-      const stripe = await getStripe()
-      const {error} = await stripe.redirectToCheckout({
+      const stripe = await getStripe();
+      const { error } = await stripe.redirectToCheckout({
         sessionId: checkoutSessionJson.id,
-      })
+      });
 
       if (error) {
-        console.warn(error.message)
+        console.warn(error.message);
       }
     }
-  }
+  };
 
-  const pricingRef=useRef()
+  const pricingRef = useRef();
   const executeScroll = () => {
     if (user && subscribed) {
-      router.push('/generate')
+      router.push("/generate");
     } else {
-      pricingRef.current?.scrollIntoView({ behavior: "smooth" })
+      pricingRef.current?.scrollIntoView({ behavior: "smooth" });
     }
-  }
-  
-
+  };
 
   return (
-      <Container maxWidth="100vw" style={{background:"lightblue"}}>
-        <Header/>
-        {/* <Head>
+    <Container maxWidth="100vw" style={{ background: "lightblue" }}>
+      <Header />
+      {/* <Head>
           <title>Flashcard GenAI</title>
           <meta name="description" content="Create flashcards from your text." />
         </Head>
@@ -110,79 +113,83 @@ export default function Home() {
             </SignedIn>
           </Toolbar>
         </AppBar> */}
-        <Box sx={{textAlign:"center", my:4}}>
-          <Typography variant="h2">
-            Welcome to Flashcard GenAI
-          </Typography>
-          <Typography variant="h5">
-            The easiest way to make flashcards from your text with the help of AI.
-          </Typography>
-          <Button variant="contained" color="primary" sx={{mt:2}} onClick={executeScroll}>
-            Get Started
-          </Button>
-        </Box>
-        <Box sx={{my:6}}>
-          <Typography variant="h4" textAlign={'center'}>
-            Features
-          </Typography>
+      <Box sx={{ textAlign: "center", my: 4 }}>
+        <Typography variant="h2">Welcome to Flashcard GenAI</Typography>
+        <Typography variant="h5">
+          The easiest way to make flashcards from your text with the help of AI.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 2 }}
+          onClick={executeScroll}
+        >
+          Get Started
+        </Button>
+      </Box>
+      <Box sx={{ my: 6 }}>
+        <Typography variant="h4" textAlign={"center"}>
+          Features
+        </Typography>
 
-          <Grid container spacing={3} sx={{mt:1}}>
-            {flashcards.map((flashcard, index) => (
-                        <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Card>
-                                <CardActionArea 
-                                    onClick={() => handleCardClick(index)}
-                                >
-                                    <CardContent>
-                                        <Box
-                                            sx={{backgroundColor:flashcard.color,
-                                                perspective: '1000px',
-                                                '& > div':{
-                                                    transition: 'transform 0.6s',
-                                                    transformStyle: 'preserve-3d',
-                                                    position: 'relative',
-                                                    width: '100%',
-                                                    height: '200px',
-                                                    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
-                                                    transform: flipped[index]?'rotateY(180deg)':'rotateY(0deg)',
-                                                },
-                                                '& > div > div':{
-                                                    position: 'absolute',
-                                                    width: '100%',
-                                                    height: '100%',
-                                                    backfaceVisibility: 'hidden',
-                                                    display: 'flex',
-                                                    justifyContent: 'center',
-                                                    alignItems: 'center',
-                                                    padding: 2,
-                                                    boxSizing: 'border-box',
-                                                },
-                                                '& > div > div:nth-of-type(2)':{
-                                                    transform: 'rotateY(180deg)',
-                                                }                                                                                                   
-                                            }}
-                                        >
-                                            <div>
-                                                <div>
-                                                    <Typography variant='h5' component='div'>
-                                                        {flashcard.front}
-                                                    </Typography>
-                                                </div>
-                                                <div>
-                                                    <Typography variant='h5' component='div'>
-                                                        {flashcard.back}
-                                                    </Typography>
-                                                </div>                                                
-                                            </div>
-                                        </Box>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Grid>
-                    ))}
+        <Grid container spacing={3} sx={{ mt: 1 }}>
+          {flashcards.map((flashcard, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Card>
+                <CardActionArea onClick={() => handleCardClick(index)}>
+                  <CardContent>
+                    <Box
+                      sx={{
+                        backgroundColor: flashcard.color,
+                        perspective: "1000px",
+                        "& > div": {
+                          transition: "transform 0.6s",
+                          transformStyle: "preserve-3d",
+                          position: "relative",
+                          width: "100%",
+                          height: "200px",
+                          boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+                          transform: flipped[index]
+                            ? "rotateY(180deg)"
+                            : "rotateY(0deg)",
+                        },
+                        "& > div > div": {
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          backfaceVisibility: "hidden",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          padding: 2,
+                          boxSizing: "border-box",
+                        },
+                        "& > div > div:nth-of-type(2)": {
+                          transform: "rotateY(180deg)",
+                        },
+                      }}
+                    >
+                      <div>
+                        <div>
+                          <Typography variant="h5" component="div">
+                            {flashcard.front}
+                          </Typography>
+                        </div>
+                        <div>
+                          <Typography variant="h5" component="div">
+                            {flashcard.back}
+                          </Typography>
+                        </div>
+                      </div>
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
             </Grid>
+          ))}
+        </Grid>
 
-          {/* <Grid container spacing={4}>
+        {/* <Grid container spacing={4}>
             <Grid item xs={12} md={4}>
               <Typography variant="h6" gutterBottom>
                 Easy text input
@@ -202,69 +209,104 @@ export default function Home() {
               <Typography>Access your flashcards from any device, at any time. Study on the go with ease.</Typography>
             </Grid>
           </Grid> */}
-        </Box>
-        <Box sx={{my:6, textAlign:'center'}}>
-          <Typography variant="h4" gutterBottom ref={pricingRef}>
-            Pricing
-          </Typography>
-          <Grid container spacing={4}>
+      </Box>
+      <Box sx={{ my: 6, textAlign: "center" }}>
+        <Typography variant="h4" gutterBottom ref={pricingRef}>
+          Pricing
+        </Typography>
+        <Grid container spacing={4}>
           <Grid item xs={12} md={4}>
-            <Box 
+            <Box
               sx={{
-                p:3,
-                border: '1px solid',
-                borderColor: 'grey.300',
+                p: 3,
+                border: "1px solid",
+                borderColor: "grey.300",
                 borderRadius: 2,
-                backgroundColor: 'red',
-                color: 'white'
-              }}>
-              <Typography variant="h5" gutterBottom>Trial</Typography>
-              <Typography variant="h6" gutterBottom>Free for a month</Typography>
-              <Typography>Try it out before purchasing. All features included for 30 days.</Typography>
-              <Button variant="contained" color="primary" sx={{mt:2}} onClick={()=>handleSubmit(0)}>
+                backgroundColor: "red",
+                color: "white",
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Trial
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                Free for a month
+              </Typography>
+              <Typography>
+                Try it out before purchasing. All features included for 30 days.
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={() => handleSubmit(0)}
+              >
                 Choose Trial
               </Button>
-              </Box>
-            </Grid>               
+            </Box>
+          </Grid>
           <Grid item xs={12} md={4}>
-            <Box 
+            <Box
               sx={{
-                p:3,
-                border: '1px solid',
-                borderColor: 'grey.300',
+                p: 3,
+                border: "1px solid",
+                borderColor: "grey.300",
                 borderRadius: 2,
-                backgroundColor: 'orange',
-                color: 'white'
-              }}>
-              <Typography variant="h5" gutterBottom>Basic</Typography>
-              <Typography variant="h6" gutterBottom>$5 / month</Typography>
-              <Typography>Access to basic flashcard features and limited storage.</Typography>
-              <Button variant="contained" color="primary" sx={{mt:2}} onClick={()=>handleSubmit(5)}>
+                backgroundColor: "orange",
+                color: "white",
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Basic
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                $5 / month
+              </Typography>
+              <Typography>
+                Access to basic flashcard features and limited storage.
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={() => handleSubmit(5)}
+              >
                 Choose Basic
               </Button>
-              </Box>
-            </Grid>            
-            <Grid item xs={12} md={4}>
-            <Box 
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box
               sx={{
-                p:3,
-                border: '1px solid',
-                borderColor: 'grey.300',
+                p: 3,
+                border: "1px solid",
+                borderColor: "grey.300",
                 borderRadius: 2,
-                backgroundColor: 'green',
-                color: 'white'
-              }}>
-              <Typography variant="h5" gutterBottom>Pro</Typography>
-              <Typography variant="h6" gutterBottom>$10 / month</Typography>
-              <Typography>Unlimited flashcards and storage, with priority support.</Typography>
-              <Button variant="contained" color="primary" sx={{mt:2}} onClick={()=>handleSubmit(10)}>
+                backgroundColor: "green",
+                color: "white",
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                Pro
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                $10 / month
+              </Typography>
+              <Typography>
+                Unlimited flashcards and storage, with priority support.
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                onClick={() => handleSubmit(10)}
+              >
                 Choose Pro
               </Button>
-              </Box>
-            </Grid>            
+            </Box>
           </Grid>
-        </Box>
-        <Typography variant="h5" textAlign="center">Designers wanted!&#128513; Apply <Link href="https://robertkiss.online/#contact" target="_blank">HERE</Link>.</Typography>
-      </Container>
-    )
+        </Grid>
+      </Box>
+    </Container>
+  );
 }
